5d53f380ff1139e6691e8b1420cfcc4dab454ef4
cesarsiguencia <cesar.siguencia@gmail.com>
2022-03-05 19:45:58 -0500

    need to commit

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
 const animalRoutes = require('../apiRoutes/animalRoutes');
 
+router.use(require('./zookeeperRoutes'));
+
+
 router.use(animalRoutes);
+
+
 
 module.exports = router;
